e209472840a4e53171bb92e2c9a3aa146397192a
Xsunr1seX <nikitataganov19@gmail.com>
2025-02-20 21:28:47 +0300

    decode added

--- headers/compression.h
+++ headers/compression.h
@@ -2,6 +2,6 @@
 #define COMPRESSION_H
 
 int encode(const char* input_filename, const char* output_filename);
-// TODO int decode(const char* input_filename, const char* output_filename);
+int decode(const char* input_filename, const char* output_filename);
 
 #endif // COMPRESSION_H
